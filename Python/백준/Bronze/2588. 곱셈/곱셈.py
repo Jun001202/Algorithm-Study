178b6fed82de60d67daed959e045c52c1c19df3f
Jun <sj1202pak@gmail.com>
2025-02-28 17:27:54 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+A = input()
+B = input()
+
+B1 = int(B[2])
+B2 = int(B[1])
+B3 = int(B[0])
+
+A_int = int(A)
+B_int = int(B)
+
+print(A_int*B1)
+print(A_int*B2)
+print(A_int*B3)
+print(A_int*B_int)
