29af5bb406f234598c67e2246d5f7a4fadb0f49b
Jun <sj1202pak@gmail.com>
2025-03-09 14:41:24 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,15 @@
+X = int(input())
+N = int(input())
+
+total = 0
+
+if 1 <= X <= 1000000000 and 1 <= N <= 100:
+    for i in range(N):
+        a, b = map(int, input().split())
+        if 1 <= a <= 1000000 and 1 <= b <= 10:
+            total += (a * b)
+
+if total == X:
+    print("Yes")
+else:
+    print("No")
